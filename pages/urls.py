a97de604e701e6542ee7c3e57e58885afc077ede
Ee Durbin <ewdurbin@gmail.com>
2024-03-11 00:59:19 -0400

    initial pass at brand guidelines page

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ from pages import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("donate", views.index, name="donate"),
+    path("brand", views.brand, name="brand"),
 ]
--- pbaabp/settings.py
+++ pbaabp/settings.py
@@ -79,13 +79,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "allauth.account.middleware.AccountMiddleware",
     "pbaabp.middleware.TimezoneMiddleware",
 ]
@@ -163,6 +163,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
