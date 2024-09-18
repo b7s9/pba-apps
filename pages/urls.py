a803db4cddf83493a60e7ee3ef48c6bc5b974f81
Ee Durbin <ewdurbin@gmail.com>
2024-09-18 08:25:49 -0400

    add privacy/conduct pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("donate", views.donate, name="donate"),
     path("brand", views.brand, name="brand"),
+    path("policies/privacy-and-data", views.privacy, name="privacy"),
+    path("policies/code-of-conduct", views.conduct, name="conduct"),
 ]
