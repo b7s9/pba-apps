4b11b04ca6eb9293930e117cd45b41f28b9bd39f
Ee Durbin <ewdurbin@gmail.com>
2024-09-19 14:18:51 -0400

    add zip code facet

--- facets/admin.py
+++ facets/admin.py
@@ -2,7 +2,7 @@ from django.contrib.gis import admin
 from django.contrib.gis.db import models
 from django.contrib.gis.forms.widgets import OSMWidget
 
-from facets.models import District, RegisteredCommunityOrganization
+from facets.models import District, RegisteredCommunityOrganization, ZipCode
 
 
 class DistrictAdmin(admin.ModelAdmin):
@@ -16,6 +16,19 @@ class DistrictAdmin(admin.ModelAdmin):
 class RegisteredCommunityOrganizationAdmin(admin.ModelAdmin):
     list_display = ["name"]
     search_fields = ["name"]
+    readonly_fields = ("zip_codes",)
+
+    formfield_overrides = {
+        models.MultiPolygonField: {"widget": OSMWidget},
+    }
+
+    def zip_codes(self, obj):
+        return ", ".join([z.name for z in obj.intersecting_zips.all()])
+
+
+class ZipCodeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
 
     formfield_overrides = {
         models.MultiPolygonField: {"widget": OSMWidget},
@@ -24,3 +37,4 @@ class RegisteredCommunityOrganizationAdmin(admin.ModelAdmin):
 
 admin.site.register(District, DistrictAdmin)
 admin.site.register(RegisteredCommunityOrganization, RegisteredCommunityOrganizationAdmin)
+admin.site.register(ZipCode, ZipCodeAdmin)
--- facets/models.py
+++ facets/models.py
@@ -30,6 +30,27 @@ class RegisteredCommunityOrganization(models.Model):
     contained_profiles = Relationship(
         to="profiles.profile", predicate=Q(location__within=L("mpoly"))
     )
+    intersecting_zips = Relationship(
+        to="facets.zipcode", predicate=Q(mpoly__intersects=L("mpoly"))
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class ZipCode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    mpoly = models.MultiPolygonField()
+    properties = models.JSONField()
+
+    targetable = models.BooleanField(default=False)
+    contained_profiles = Relationship(
+        to="profiles.profile", predicate=Q(location__within=L("mpoly"))
+    )
+    intersecting_rcos = Relationship(
+        to="facets.registeredcommunityorganization", predicate=Q(mpoly__intersects=L("mpoly"))
+    )
 
     def __str__(self):
         return self.name
