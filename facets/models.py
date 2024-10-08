c7a6b5c68a745d8e8da06b0ab8a099343f838f9c
Ee Durbin <ewdurbin@gmail.com>
2024-10-08 16:47:43 -0400

    add PA House/Senate Districts as facets

--- facets/models.py
+++ facets/models.py
@@ -5,31 +5,30 @@ from django.db.models import Q
 from relativity.fields import L, Relationship
 
 
-class District(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
-    mpoly = models.MultiPolygonField()
-    properties = models.JSONField()
-
-    targetable = models.BooleanField(default=True)
-    contained_profiles = Relationship(
-        to="profiles.profile", predicate=Q(location__within=L("mpoly"))
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class RegisteredCommunityOrganization(models.Model):
+class Facet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
     mpoly = models.MultiPolygonField()
     properties = models.JSONField()
 
     targetable = models.BooleanField(default=False)
+
     contained_profiles = Relationship(
         to="profiles.profile", predicate=Q(location__within=L("mpoly"))
     )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class District(Facet):
+    targetable = models.BooleanField(default=True)
+
+
+class RegisteredCommunityOrganization(Facet):
     intersecting_zips = Relationship(
         to="facets.zipcode", predicate=Q(mpoly__intersects=L("mpoly"))
     )
@@ -42,23 +41,16 @@ class RegisteredCommunityOrganization(models.Model):
             if z.mpoly.intersection(self.mpoly).area / self.mpoly.area > 0.001
         ]
 
-    def __str__(self):
-        return self.name
 
-
-class ZipCode(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
-    mpoly = models.MultiPolygonField()
-    properties = models.JSONField()
-
-    targetable = models.BooleanField(default=False)
-    contained_profiles = Relationship(
-        to="profiles.profile", predicate=Q(location__within=L("mpoly"))
-    )
+class ZipCode(Facet):
     intersecting_rcos = Relationship(
         to="facets.registeredcommunityorganization", predicate=Q(mpoly__intersects=L("mpoly"))
     )
 
-    def __str__(self):
-        return self.name
+
+class StateHouseDistrict(Facet):
+    pass
+
+
+class StateSenateDistrict(Facet):
+    pass
