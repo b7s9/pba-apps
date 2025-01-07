59e18f5a56fdc1e2f2cfa83f2d8f1d86817275f3
Ee Durbin <ewdurbin@gmail.com>
2025-01-07 15:51:36 -0500

    make form more formy

--- projects/forms.py
+++ projects/forms.py
@@ -8,11 +8,11 @@ class ProjectApplicationForm(forms.Form):
         return {field.name: {"label": field.label, "value": field.value()} for field in self}
 
     shortname = forms.CharField(
-        label="Shortname", max_length=128, help_text="A short name for your project", required=True
+        label="Shortname", max_length=256, help_text="A short name for your project", required=True
     )
     quick_summary = forms.CharField(
         label="Quick Summary",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         help_text="Briefly describe your project",
         required=True,
     )
@@ -32,7 +32,7 @@ class ProjectApplicationForm(forms.Form):
     )
     leader_past_experience = forms.CharField(
         label="Please describe any past experience relevant to this project",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
 
@@ -42,35 +42,35 @@ class ProjectApplicationForm(forms.Form):
             "How does this project advance PBA's mission "
             "to improve the bicycle infrastructure of Philadelphia?"
         ),
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     success_criteria = forms.CharField(
         label="What are the success criteria for this project? How will we know it's done?",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     name_use = forms.CharField(
         label="Will this project be branded and marketed under PBA's name?",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     recruitment = forms.CharField(
         label=(
             "Have you already recruited anyone in PBA to help you with this project?" "If so, who?"
         ),
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     external_orgs = forms.CharField(
         label="Will any external organizations be collaborating on this project?",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
 
     # logistics
     time_and_date = forms.CharField(
-        label="Time and date (if applicable)", max_length=128, required=False
+        label="Time and date (if applicable)", max_length=256, required=False
     )
     recurring = forms.BooleanField(
         label="Is this a recurring event?",
@@ -82,40 +82,40 @@ class ProjectApplicationForm(forms.Form):
     )
     location = forms.CharField(
         label="Location (if applicable)",
-        max_length=128,
+        max_length=256,
         required=False,
     )
 
     # resources
     equipment_needed = forms.CharField(
         label="Describe equipment needed (tables, banners, trailers, etc.)",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     volunteers_needed = forms.CharField(
         label="Describe volunteer resources needed",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     promotion_needed = forms.CharField(
         label="Describe promotional needs (Instagram, mailing list, etc.)",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     finances_needed = forms.CharField(
         label="Describe financial/money needs",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=True,
     )
     others_needed = forms.CharField(
         label="Describe other needs",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=False,
     )
 
     # anything else
     anything_else = forms.CharField(
         label="Anything else you want to include in your application?",
-        max_length=2048,
+        widget=forms.Textarea(attrs={"rows": 3}),
         required=False,
     )
