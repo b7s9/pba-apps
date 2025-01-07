9f9c5a76f76959ea7c0a2c73477a4d8ccfaf08c7
Ee Durbin <ewdurbin@gmail.com>
2025-01-07 16:23:03 -0500

    add script for email send tomorrow

---
+++ pbaabp/management/commands/send_email_fishtown_neighborhood_bikeways_2025_01_07.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from campaigns.models import PetitionSignature
+from pbaabp.email import send_email_message
+from profiles.models import Profile
+
+TO = ["bikes@durbin.ee"]
+
+profiles = Profile.objects.filter(zip_code__in=["TEST LOL"])
+
+signatures = []
+profiles = Profile.objects.filter(user__email__in=TO)
+
+SENT = []
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("email_template", nargs="?", type=str)
+
+    def handle(self, *args, **options):
+        settings.EMAIL_SUBJECT_PREFIX = ""
+        for profile in profiles:
+            if profile.user.email not in SENT:
+                send_email_message(
+                    "fishtown_neighborhood_bikeways_2025_01_07",
+                    "Philly Bike Action <noreply@bikeaction.org>",
+                    [profile.user.email],
+                    {"profile": profile},
+                    reply_to=["info@bikeaction.org"],
+                )
+                SENT.append(profile.user.email)
+            else:
+                print(f"skipping {profile}")
+        print(len(SENT))
