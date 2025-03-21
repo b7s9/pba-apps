63c87703c4177e32691a54838031ce12d1c1a42e
Ee Durbin <ewdurbin@gmail.com>
2025-03-21 06:24:19 -0400

    make email login a little prettier and obvious

--- pbaabp/forms.py
+++ pbaabp/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class EmailLoginForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        help_text="The email address you used to sign up for your Philly Bike Action Account"
+    )
