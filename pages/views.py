08d9d34ed29d49a543496dce1ca4d4808f7166a5
Ee Durbin <ewdurbin@gmail.com>
2025-03-27 12:59:59 -0400

    remove defunct pages/views, they're in cms now

--- pages/views.py
+++ pages/views.py
@@ -25,21 +25,5 @@ def index(request, *args, **kwargs):
     return render(request, "index.html")
 
 
-def donate(request):
-    return render(request, "donate.html")
-
-
 def brand(request):
     return render(request, "brand-guidelines.html")
-
-
-def privacy(request):
-    with open("pages/templates/privacy-and-data-policy.md") as f:
-        markdown = f.read()
-    return render(request, "markdown.html", context={"markdown": markdown})
-
-
-def conduct(request):
-    with open("pages/templates/code-of-conduct.md") as f:
-        markdown = f.read()
-    return render(request, "markdown.html", context={"markdown": markdown})
