a982669a32cec801b92ce35a51cdfdb2dddec413
Ee Durbin <ewdurbin@gmail.com>
2025-03-27 12:58:44 -0400

    add support for transitioning WP QR codes to apps

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import redirect, render
 from wagtail.models import Site
 
 from cms.models import HomePage
+from pages.models import LegacyQRCode, LegacyQRCodeScan
 
 
 def index(request, *args, **kwargs):
+    # Handle legacy qr codes, which were sent to the index page 💀
+    # /?rest_route=/rapi/dynamic-qr-code&qr=take-the-lane-rsvp
+    if request.GET.get("rest_route", None) == "/rapi/dynamic-qr-code":
+        if qr := request.GET.get("qr", None):
+            qr_code = LegacyQRCode.objects.filter(active=True, key=qr).first()
+            if qr_code:
+                LegacyQRCodeScan.objects.create(qr_code=qr_code)
+                return redirect(qr_code.target)
+
     if request.get_host() == "abp.bikeaction.org":
         site = Site.find_for_request(request)
         root_page = site.root_page
