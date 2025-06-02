2736bc6404118ac74342dc220b3859a28894b2dd
Ee Durbin <ewdurbin@gmail.com>
2025-06-02 06:34:44 -0400

    lint

--- pbaabp/management/commands/send_sp_postcards.py
+++ pbaabp/management/commands/send_sp_postcards.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.db.models import Q
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from facets.models import District
 from pbaabp.email import send_email_message
@@ -19,7 +19,9 @@ class Command(BaseCommand):
         SENT = []
         settings.EMAIL_SUBJECT_PREFIX = ""
         SENT = []
-        for profile in Profile.objects.filter(Q(location__within=district1.mpoly) | Q(location__within=district2.mpoly)):
+        for profile in Profile.objects.filter(
+            Q(location__within=district1.mpoly) | Q(location__within=district2.mpoly)
+        ):
             if profile.user.email not in SENT:
                 send_email_message(
                     "sp-postcards",
