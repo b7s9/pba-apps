2c6b62596b945c46e4f07e5aa541f36d7da9e769
Ee Durbin <ewdurbin@gmail.com>
2025-06-26 08:31:52 -0400

    wrap approve/reject component callbacks in a lock

--- pba_discord/components/laser_vision.py
+++ pba_discord/components/laser_vision.py
@@ -1,7 +1,9 @@
 import re
 
+from django.core.cache import cache
 from interactions import Extension, component_callback
 
+from lazer.models import ViolationReport
 from lazer.tasks import submit_violation_report_to_ppa
 from lazer.utils import build_embed
 
@@ -14,58 +16,55 @@ class LaserVision(Extension):
 
     @component_callback(APPROVE_BUTTON_ID_REGEX)
     async def approve_callback(self, ctx):
-        from lazer.models import ViolationReport
-
         violation_id = ctx.custom_id.replace("laser_violation_approve_", "")
-        violation_report = (
-            await ViolationReport.objects.filter(id=violation_id)
-            .select_related("submission")
-            .afirst()
-        )
-        if violation_report is None:
-            await ctx.send("No violation report found!", ephemeral=True)
-            await ctx.edit_origin(components=[])
-            return
+        with cache.lock(f"laser-violation-report-{violation_id}"):
+            violation_report = (
+                await ViolationReport.objects.filter(id=violation_id)
+                .select_related("submission")
+                .afirst()
+            )
+            if violation_report is None:
+                await ctx.send("No violation report found!", ephemeral=True)
+                await ctx.edit_origin(components=[])
+                return
 
-        if violation_report.submitted is not None:
-            await ctx.send("Violation report already submitted!", ephemeral=True)
-            await ctx.edit_origin(components=[])
-            return
+            if violation_report.submitted is not None:
+                await ctx.send("Violation report already submitted!", ephemeral=True)
+                await ctx.edit_origin(components=[])
+                return
 
-        submit_violation_report_to_ppa.delay(violation_report.id)
+            submit_violation_report_to_ppa.delay(violation_report.id)
 
-        embed = build_embed(violation_report)
-        embed.description = f"**VIOLATION REPORT APPROVED by {ctx.member}**"
-        await ctx.edit_origin(embeds=[embed], components=[])
-        await ctx.send("Violation report approved and submitted!", ephemeral=True)
+            embed = build_embed(violation_report)
+            embed.description = f"**VIOLATION REPORT APPROVED by {ctx.member}**"
+            await ctx.edit_origin(embeds=[embed], components=[])
+            await ctx.send("Violation report approved and submitted!", ephemeral=True)
 
     REJECT_BUTTON_ID_REGEX = re.compile(r"laser_violation_reject_(.*)")
 
     @component_callback(REJECT_BUTTON_ID_REGEX)
     async def reject_callback(self, ctx):
-        print("here")
-        from lazer.models import ViolationReport
-
         violation_id = ctx.custom_id.replace("laser_violation_reject_", "")
-        violation_report = (
-            await ViolationReport.objects.filter(id=violation_id)
-            .select_related("submission")
-            .afirst()
-        )
-        if violation_report is None:
-            await ctx.send("No violation report found!", ephemeral=True)
-            await ctx.edit_origin(components=[])
-            return
+        with cache.lock(f"laser-violation-report-{violation_id}"):
+            violation_report = (
+                await ViolationReport.objects.filter(id=violation_id)
+                .select_related("submission")
+                .afirst()
+            )
+            if violation_report is None:
+                await ctx.send("No violation report found!", ephemeral=True)
+                await ctx.edit_origin(components=[])
+                return
 
-        if violation_report.submitted is not None:
-            await ctx.send("Violation report already submitted!", ephemeral=True)
-            await ctx.edit_origin(components=[])
-            return
+            if violation_report.submitted is not None:
+                await ctx.send("Violation report already submitted!", ephemeral=True)
+                await ctx.edit_origin(components=[])
+                return
 
-        embed = build_embed(violation_report)
-        embed.description = f"**VIOLATION REPORT REJECTED by {ctx.member}**"
-        await ctx.edit_origin(embeds=[embed], components=[])
-        await ctx.send("Violation report rejected!", ephemeral=True)
+            embed = build_embed(violation_report)
+            embed.description = f"**VIOLATION REPORT REJECTED by {ctx.member}**"
+            await ctx.edit_origin(embeds=[embed], components=[])
+            await ctx.send("Violation report rejected!", ephemeral=True)
 
 
 def setup(bot):
